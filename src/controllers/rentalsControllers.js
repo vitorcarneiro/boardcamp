7ac7e6e33d0159e29c3415bce374e104a6f3a59b
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-03-07 17:00:56 -0300

    feat: end a rental

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -33,3 +33,64 @@ export async function createRental(req, res) {
         res.status(500).send(error);
     }
 }
+
+export async function readRentals(req, res) {
+    const idRegex = /^[0-9]*$/;
+
+    let customerIdQuery = '';
+    if (req.query.customerId) {
+        if (!idRegex.test(req.query.customerId)) { return res.status(400).send("customerId query must be a number")}
+        customerIdQuery = `"customerId"=${req.query.customerId}`;
+    }
+
+    let gameIdQuery = '';
+    if (req.query.gameId) {
+        if (!idRegex.test(req.query.gameId)) { return res.status(400).send("gameId query must be a number")}
+        gameIdQuery = `"gameId"=${req.query.gameId}`;
+    }
+
+    let clauseSql = '';
+    if (req.query.customerId || req.query.gameId) {
+        clauseSql = `WHERE`;
+    }
+
+    let logicalOperator = '';
+    if (req.query.customerId && req.query.gameId) {
+        logicalOperator = `AND`;
+    }
+
+    try {
+        const rentalsList = await connection.query(`
+            SELECT * FROM rentals ${clauseSql} ${customerIdQuery} ${logicalOperator} ${gameIdQuery}
+        `);
+        return res.send(rentalsList.rows);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export async function endRental(req, res) {
+    const { id } = req.params;
+    const returnDate = dayjs().format('YYYY-MM-DD');
+    let delayFee = 0;
+
+    try {
+        const rental = await connection.query(`SELECT * FROM rentals WHERE id=$1`, [id]);
+        if (rental.rowCount === 0) { return res.status(404).send("rental id not found"); }
+        if (rental.rows[0].returnDate) { return res.status(400).send("rental is already ended"); }
+
+        const daysWithFee = dayjs(rental.rows[0].rentDate).add(rental.rows[0].daysRented, 'day').diff(returnDate, 'day') * -1;
+        if (daysWithFee > 0) {
+            delayFee = daysWithFee * (rental.rows[0].originalPrice / rental.rows[0].daysRented);
+        }
+
+        await connection.query(`
+            UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3`, [returnDate, delayFee, id]);
+
+        return res.sendStatus(200);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- src/routes/rentalsRouters.js
+++ src/routes/rentalsRouters.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createRental } from "../controllers/rentalsControllers.js"
+import { createRental, readRentals, endRental } from "../controllers/rentalsControllers.js"
 import { createRentalValidationMiddleware } from "../middlewares/rentalsMiddlewares.js";
 
 const rentalsRouters = Router();
 rentalsRouters.post("/rentals", createRentalValidationMiddleware, createRental);
+rentalsRouters.get("/rentals", readRentals);
+rentalsRouters.post("/rentals/:id/return", endRental);
+
 
 export default rentalsRouters;
